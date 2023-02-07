49066ab4c55f27c30d3a5906caab4a8321456522
ClaytonDewey <clay@dryadmedia.com>
2023-02-07 15:41:27 -0600

    Create the UserMenu component

---
+++ src/components/Navbar/RightContent/UserMenu.tsx
@@ -0,0 +1,78 @@
+import {
+  MenuButton,
+  MenuList,
+  MenuItem,
+  Menu,
+  Icon,
+  Flex,
+  MenuDivider,
+} from '@chakra-ui/react';
+import { signOut, User } from 'firebase/auth';
+import React from 'react';
+import { FaRedditSquare } from 'react-icons/fa';
+import { VscAccount } from 'react-icons/vsc';
+import { IoSparkles } from 'react-icons/io5';
+import { CgProfile } from 'react-icons/cg';
+import { MdOutlineLogin } from 'react-icons/md';
+import { ChevronDownIcon } from '@chakra-ui/icons';
+import { auth } from '@/src/firebase/clientApp';
+
+type UserMenuProps = {
+  user?: User | null;
+};
+
+const UserMenu: React.FC<UserMenuProps> = ({ user }) => {
+  return (
+    <Menu>
+      <MenuButton
+        cursor='pointer'
+        padding='0px 6px'
+        borderRadius={4}
+        _hover={{ outline: '1px solid', outlineColor: 'gray.200' }}
+      >
+        {user ? (
+          <Flex align='center'>
+            <Flex align='center'>
+              <>
+                <Icon
+                  as={FaRedditSquare}
+                  fontSize={24}
+                  mr={1}
+                  color='gray.300'
+                />
+              </>
+              <ChevronDownIcon />
+            </Flex>
+          </Flex>
+        ) : (
+          <Icon fontSize={24} color='gray.400' mr={1} as={VscAccount} />
+        )}
+      </MenuButton>
+      <MenuList>
+        <MenuItem
+          fontSize='10pt'
+          fontWeight={700}
+          _hover={{ bg: 'blue.500', color: 'white' }}
+        >
+          <Flex align='center'>
+            <Icon fontSize={20} mr={2} as={CgProfile} />
+            Profile
+          </Flex>
+        </MenuItem>
+        <MenuDivider />
+        <MenuItem
+          fontSize='10pt'
+          fontWeight={700}
+          _hover={{ bg: 'blue.500', color: 'white' }}
+          onClick={() => signOut(auth)}
+        >
+          <Flex align='center'>
+            <Icon fontSize={20} mr={2} as={MdOutlineLogin} />
+            Logout
+          </Flex>
+        </MenuItem>
+      </MenuList>
+    </Menu>
+  );
+};
+export default UserMenu;
