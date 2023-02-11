457ce337a44cc4e1cf5c9d0d41eaa0ad962e05a0
Clayton Dewey <clay@dryadmedia.com>
2023-02-11 13:06:51 -0600

    Update logout function

--- src/components/Navbar/RightContent/UserMenu.tsx
+++ src/components/Navbar/RightContent/UserMenu.tsx
@@ -17,15 +17,23 @@ import { CgProfile } from 'react-icons/cg';
 import { MdOutlineLogin } from 'react-icons/md';
 import { ChevronDownIcon } from '@chakra-ui/icons';
 import { auth } from '@/src/firebase/clientApp';
-import { useSetRecoilState } from 'recoil';
+import { useResetRecoilState, useSetRecoilState } from 'recoil';
 import { authModalState } from '@/src/atoms/authModalAtom';
+import { communityState } from '@/src/atoms/communitiesAtom';
 
 type UserMenuProps = {
   user?: User | null;
 };
 
 const UserMenu: React.FC<UserMenuProps> = ({ user }) => {
+  const resetCommunityState = useResetRecoilState(communityState);
   const setAuthModalState = useSetRecoilState(authModalState);
+
+  const logout = async () => {
+    await signOut(auth);
+    // clear community state
+    resetCommunityState();
+  };
 
   return (
     <Menu>
@@ -33,8 +41,7 @@ const UserMenu: React.FC<UserMenuProps> = ({ user }) => {
         cursor='pointer'
         padding='0px 6px'
         borderRadius={4}
-        _hover={{ outline: '1px solid', outlineColor: 'gray.200' }}
-      >
+        _hover={{ outline: '1px solid', outlineColor: 'gray.200' }}>
         <Flex align='center'>
           <Flex align='center'>
             {user ? (
@@ -50,8 +57,7 @@ const UserMenu: React.FC<UserMenuProps> = ({ user }) => {
                   display={{ base: 'none', lg: 'flex' }}
                   fontSize='8pt'
                   align='flex-start'
-                  mr={8}
-                >
+                  mr={8}>
                   <Text fontWeight={700}>
                     {user?.displayName || user.email?.split('@')[0]}
                   </Text>
@@ -74,8 +80,7 @@ const UserMenu: React.FC<UserMenuProps> = ({ user }) => {
             <MenuItem
               fontSize='10pt'
               fontWeight={700}
-              _hover={{ bg: 'blue.500', color: 'white' }}
-            >
+              _hover={{ bg: 'blue.500', color: 'white' }}>
               <Flex align='center'>
                 <Icon fontSize={20} mr={2} as={CgProfile} />
                 Profile
@@ -86,8 +91,7 @@ const UserMenu: React.FC<UserMenuProps> = ({ user }) => {
               fontSize='10pt'
               fontWeight={700}
               _hover={{ bg: 'blue.500', color: 'white' }}
-              onClick={() => signOut(auth)}
-            >
+              onClick={logout}>
               <Flex align='center'>
                 <Icon fontSize={20} mr={2} as={MdOutlineLogin} />
                 Logout
@@ -100,8 +104,7 @@ const UserMenu: React.FC<UserMenuProps> = ({ user }) => {
               fontSize='10pt'
               fontWeight={700}
               _hover={{ bg: 'blue.500', color: 'white' }}
-              onClick={() => setAuthModalState({ open: true, view: 'login' })}
-            >
+              onClick={() => setAuthModalState({ open: true, view: 'login' })}>
               <Flex align='center'>
                 <Icon fontSize={20} mr={2} as={MdOutlineLogin} />
                 Log In / Sign Up
