f73d840b6dc1f520d9a66d1b32a3ec4a8f60537f
ClaytonDewey <clay@dryadmedia.com>
2023-02-14 14:53:11 -0600

    Create single post page.

---
+++ src/pages/r/[communityId]/comments/[pid].tsx
@@ -0,0 +1,49 @@
+import PageContent from '@/src/components/Layout/PageContent';
+import PostItem from '@/src/components/Posts/PostItem';
+import { auth } from '@/src/firebase/clientApp';
+import usePosts from '@/src/hooks/usePosts';
+import { useRouter } from 'next/router';
+import React, { useEffect } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+
+const PostPage: React.FC = () => {
+  const [user] = useAuthState(auth);
+  const {
+    postStateValue,
+    setPostStateValue,
+    onDeletePost,
+    onVote,
+  } = usePosts();
+  const router = useRouter();
+
+  const fetchPosts = async (postId: string) => {};
+
+  useEffect(() => {}, []);
+
+  return (
+    <PageContent>
+      <>
+        {postStateValue.selectedPost && (
+          <PostItem
+            post={postStateValue.selectedPost}
+            onVote={onVote}
+            onDeletePost={onDeletePost}
+            userVoteValue={
+              postStateValue.postVotes.find(
+                (item) => item.postId === postStateValue.selectedPost?.id
+              )?.voteValue
+            }
+            userIsCreator={user?.uid === postStateValue.selectedPost?.creatorId}
+          />
+        )}
+        {/* Selectedpost */}
+        {/* comments */}
+      </>
+      <>
+        RHS
+        {/* <About /> */}
+      </>
+    </PageContent>
+  );
+};
+export default PostPage;
