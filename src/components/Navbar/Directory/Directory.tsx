a9d32ded167405f2e39dd81f77792049fbc2df37
ClaytonDewey <clay@dryadmedia.com>
2023-02-16 11:24:49 -0600

    Add toggleState

--- src/components/Navbar/Directory/Directory.tsx
+++ src/components/Navbar/Directory/Directory.tsx
@@ -1,3 +1,4 @@
+import useDirectory from '@/src/hooks/useDirectory';
 import { ChevronDownIcon } from '@chakra-ui/icons';
 import { Flex, Icon, Menu, MenuButton, MenuList, Text } from '@chakra-ui/react';
 import React from 'react';
@@ -5,8 +6,10 @@ import { TiHome } from 'react-icons/ti';
 import Communities from './Communities';
 
 const UserMenu: React.FC = () => {
+  const { directoryState, toggleMenuOpen } = useDirectory();
+
   return (
-    <Menu>
+    <Menu isOpen={directoryState.isOpen}>
       <MenuButton
         cursor='pointer'
         padding='0px 6px'
@@ -14,6 +17,7 @@ const UserMenu: React.FC = () => {
         mr={2}
         ml={{ base: 0, md: 2 }}
         _hover={{ outline: '1px solid', outlineColor: 'gray.200' }}
+        onClick={toggleMenuOpen}
       >
         <Flex
           align='center'
