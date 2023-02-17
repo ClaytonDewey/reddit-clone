4c432f3fe06bb7a31d9d4066f0f9f9161be2c009
ClaytonDewey <clay@dryadmedia.com>
2023-02-17 14:32:18 -0600

    Rewrite import

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,7 @@ import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
 import { RecoilRoot } from 'recoil';
 import { theme } from '../chakra/theme';
-import Layout from '../components/Layout/Layout';
-// What's going on...
+import Layout from '../components/Layout/';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
