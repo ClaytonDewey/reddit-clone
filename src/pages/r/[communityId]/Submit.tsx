4a5993b092e7e89e0f86805ffdc5d100b165b4bd
ClaytonDewey <clay@dryadmedia.com>
2023-02-17 14:36:25 -0600

    Update Submit form

--- src/pages/r/[communityId]/Submit.tsx
+++ src/pages/r/[communityId]/Submit.tsx
@@ -1,36 +1,62 @@
-import { communityState } from '@/src/atoms/communitiesAtom';
-import About from '@/src/components/Community/About';
-import PageContent from '@/src/components/Layout/PageContent';
-import NewPostForm from '@/src/components/Posts/NewPostForm';
-import { auth } from '@/src/firebase/clientApp';
-import useCommunityData from '@/src/hooks/useCommunityData';
 import { Box, Text } from '@chakra-ui/react';
-import React from 'react';
+import { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { useRecoilValue } from 'recoil';
+import { communityState } from '../../../atoms/communitiesAtom';
+import About from '../../../components/Community/About';
+import PageContentLayout from '../../../components/Layout/PageContent';
+import NewPostForm from '@/src/components/Posts/NewPostForm';
+import { auth } from '../../../firebase/clientApp';
+import useCommunityData from '../../../hooks/useCommunityData';
 
-const SubmitPostPage: React.FC = () => {
-  const [user] = useAuthState(auth);
-  const { communityStateValue } = useCommunityData();
-  // console.log('COMMUNITY', communityStateValue);
+const CreateCommmunityPostPage: NextPage = () => {
+  const [user, loadingUser, error] = useAuthState(auth);
+  const router = useRouter();
+  const { community } = router.query;
+  // const visitedCommunities = useRecoilValue(communityState).visitedCommunities;
+  const communityStateValue = useRecoilValue(communityState);
+  const { loading } = useCommunityData();
+
+  /**
+   * Not sure why not working
+   * Attempting to redirect user if not authenticated
+   */
+  useEffect(() => {
+    if (!user && !loadingUser && communityStateValue.currentCommunity.id) {
+      router.push(`/r/${communityStateValue.currentCommunity.id}`);
+    }
+  }, [user, loadingUser, communityStateValue.currentCommunity]);
+
+  console.log('HERE IS USER', user, loadingUser);
+
   return (
-    <PageContent>
+    <PageContentLayout maxWidth='1060px'>
       <>
         <Box p='14px 0px' borderBottom='1px solid' borderColor='white'>
-          <Text>Create a post</Text>
+          <Text fontWeight={600}>Create a post</Text>
         </Box>
         {user && (
           <NewPostForm
+            communityId={communityStateValue.currentCommunity.id}
+            communityImageURL={communityStateValue.currentCommunity.imageURL}
             user={user}
-            communityImageURL={communityStateValue.currentCommunity?.imageURL}
           />
         )}
       </>
-      <>
-        {communityStateValue.currentCommunity && (
-          <About communityData={communityStateValue.currentCommunity} />
-        )}
-      </>
-    </PageContent>
+      {communityStateValue.currentCommunity && (
+        <>
+          <About
+            communityData={communityStateValue.currentCommunity}
+            pt={6}
+            onCreatePage
+            loading={loading}
+          />
+        </>
+      )}
+    </PageContentLayout>
   );
 };
-export default SubmitPostPage;
+
+export default CreateCommmunityPostPage;
